bd379fec9f1d925bd5d35208de8e665071c99e25
chao zhu <chaozhu@Sona.local>
2015-01-05 18:40:33 -0500

    commit

---
+++ testProject/testProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testProject
+//
+//  Created by chao zhu on 2015-01-05.
+//  Copyright (c) 2015 chao zhu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
